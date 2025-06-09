eba6bdf8c2a0fb7c9ae8992476cdb2c654eb9228
isachim <164205782+isachim@users.noreply.github.com>
2025-06-09 10:35:38 -0300

    Update mais.py

--- mais.py
+++ mais.py
@@ -1,19 +1,28 @@
-url = "https://bytebank.com/cambio?moeda0rigem=real"
-print(url)
+#url = "https://bytebank.com/cambio?moeda0rigem=real"
+url = ""
 
+# Sanitização da url
+url = url.replace("","")
+
+# Validação da url
+if url =="":
+    raise ValueError("A url etá vazia")
+
+# Separa base e parametros
 indice_interrogação= url.find('?')
 url_base = url[0:19]
 print(url_base)
-
 url_parametros = url[20:36]
 print(url_parametros)
 
-parametro_busca = 'moeda)rigem'
+# Busca o valor de um paramero
+parametro_busca = 'mquantidade'
 indice_parametro = url_paramentros.find(parametro_busca)
-
-print(indice_parametro)
-
 indice_valor = indice_parametro + indice_tamanho + 1
-valor = url_parametro[indice_valor:]
+indice_e_comercial = url_parametros.find('&')
+if indice_e_comercial ==-1:
+    valor = url_parametro[indice_valor:]
+else:
+   valor= url_parametro[indice_valor:indice_e_comercial]
 
 print(valor)
