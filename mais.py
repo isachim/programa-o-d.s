026e1204c7f0d3687e36cb00177dca4c652ce26f
isachim <164205782+isachim@users.noreply.github.com>
2025-06-11 12:13:28 -0300

    Update mais.py

--- mais.py
+++ mais.py
@@ -26,3 +26,8 @@ else:
    valor= url_parametro[indice_valor:indice_e_comercial]
 
 print(valor)
+
+# extrator_url = ExtratorURL("bytebank.com/cambio?quantidade =100moedaOrigem=real&moedaDestino=dolar")
+extrator_url = ExtratorURL("   ")
+# valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+# print(valor_quantidade)
